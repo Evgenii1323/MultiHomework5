8e9d97dfa29adb7825eda55577ae12c53ad1d1f5
Evgenii Vasilev <bk6789@bk.ru>
2022-12-15 22:02:13 +0700

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,3 +1,16 @@
-public class PhoneBookTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+public class PhoneBookTest {
+    @Test
+    public void testAdd() {
+        String name = "Jack";
+        String phone = "12345";
+        int expected = 1;
+
+        PhoneBook phoneBook = new PhoneBook();
+        int result = phoneBook.add(name, phone);
+
+        Assertions.assertEquals(expected, result);
+    }
 }
